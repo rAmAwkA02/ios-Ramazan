e4caa3fed273237b67b327a7a1f9e052d1a99b2b
Ramazan Amangeldin <ramazanamangeldin@MacBook-Pro-Ramazan.local>
2023-02-23 18:58:54 +0600

    Add login screen

---
+++ BankeyRoma/BankeyRoma/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  BankeyRoma
+//
+//  Created by Ramazan Amangeldin on 20/02/2023.
+//
+
+import Foundation
---
+++ BankeyRoma/BankeyRoma/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankeyRoma
+//
+//  Created by Ramazan Amangeldin on 20/02/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
